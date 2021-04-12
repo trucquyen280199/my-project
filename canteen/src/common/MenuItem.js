21f4afcdd1cd4d15051fceab2e1f9b4fc42d57f5
DESKTOP-17ORS73\Dell E5470 <trucquyen280199@gmail.com>
2021-04-12 10:12:45 +0700

    canteen

---
+++ canteen/src/common/MenuItem.js
@@ -0,0 +1,38 @@
+import React from "react";
+import './menu_item.css'
+/* eslint-disable */
+class MenuItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSelect: false
+        }
+        this.setSelect = this.setSelect.bind(this);
+    }
+
+    setSelect(action) {
+        if (action === this.props.currentAction) {
+            this.setState({isSelect: true})
+        } else {
+            this.setState({isSelect: false})
+        }
+    }
+
+    action = () => {
+        this.props.action(this.props.linkTo)
+    }
+
+    render() {
+        var classCss = "button_menu"
+        if (this.props.currentAction === this.props.linkTo) {
+            classCss = "button_menu_open"
+        }
+        return (
+            <React.Fragment>
+                <a onClick={this.action} className={classCss}>{this.props.menuName}</a>
+            </React.Fragment>
+        )
+    }
+}
+
+export default MenuItem
