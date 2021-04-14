1987ccf5813cb4ea78f8611f46b508ac174a493c
DESKTOP-17ORS73\Dell E5470 <trucquyen280199@gmail.com>
2021-04-14 11:43:04 +0700

    tools-master

--- device/src/pages/Access Control/user-group.js
+++ device/src/pages/Access Control/user-group.js
@@ -4,8 +4,7 @@ function UserGroup(){
     return(
         <div className="pageTest notfound" style={{background: "url(./images/bg-404.jpg)"}}>
             <nav className="toc d-none">
-                <ul className="menu-main">
-                </ul>
+                <ul className="menu-main"></ul>
                 <svg className="toc-marker" width="200" height="200" xmlns="http://www.w3.org/2000/svg">
                     <path stroke="#444" stroke-width="3" fill="transparent" stroke-dasharray="0, 0, 0, 1000" stroke-linecap="round" stroke-linejoin="round" transform="translate(-0.5, -0.5)" />
                 </svg>
